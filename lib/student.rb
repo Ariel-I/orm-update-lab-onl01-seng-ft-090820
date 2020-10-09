71c56e16c04c7c5c196ca4e200f837234a00fb20
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-09 01:11:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,11 +56,10 @@ class Student
   
   
   def self.new_from_db(row)
-    self.new(id, name, grade)
     id = row[0]
     name = row [1]
     grade = row[2]
-    
+    self.new(id, name, grade)
   end 
   
   
